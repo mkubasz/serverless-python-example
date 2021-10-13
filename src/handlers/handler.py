f12a50586054fce0113cf82c9185a1ca86aacc12
Mateusz <mkubasz@gmail.com>
2021-10-13 23:19:06 +0200

    feat: add fast api

--- src/handlers/handler.py
+++ src/handlers/handler.py
@@ -1,24 +1,11 @@
-import json
+from fastapi import FastAPI
+from mangum import Mangum
+
+app = FastAPI()
 
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+@app.get('/')
+def pong():
+    return {'Ping': 'pong'}
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-
-    return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+api = Mangum(app)
